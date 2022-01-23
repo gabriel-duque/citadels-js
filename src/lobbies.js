a7fe9ee938024cfda255c591e040047b7cb79e46
ShallowRed <48523123+ShallowRed@users.noreply.github.com>
2022-01-23 19:51:17 +0100

    misc

--- src/lobbies.js
+++ src/lobbies.js
@@ -1,24 +1,63 @@
 import './lobby.css';
 
-console.log("aaa")
+const gameName = document.querySelector('meta[name="game-name"]')
+  .content;
+
+
+document.querySelectorAll('.lobby').forEach(lobbyElement => {
+  attachListener(lobbyElement);
+});
 
 const createLobbyButton = document.querySelector('.create-lobby');
 
 createLobbyButton.addEventListener('click', () => {
 
-  // send request to server
+  postRequest('/create-lobby/' + gameName, response => {
+    renderLobby(response)
+  })
+});
+
+function postRequest(route, onload) {
+
   const request = new XMLHttpRequest();
-  request.open('POST', '/lobbies');
+  request.open('POST', route);
   request.setRequestHeader('Content-Type', 'application/json');
   request.send();
-
-  request.onload = () => {
-    const lobby = JSON.parse(request.responseText);
-    console.log(lobby);
-  }
 
   request.onerror = () => {
     console.log('error');
   }
 
-});
+  return request.onload = () => onload(JSON.parse(request.responseText));
+}
+
+function renderLobby(lobby) {
+
+  const lobbyElement = document.createElement('div');
+  const lobbyId = document.createElement("p");
+  const joinButton = document.createElement("button");
+
+  lobbyElement.classList.add('lobby');
+
+  lobbyId.innerHTML = lobby.lobbyId;
+  joinButton.innerHTML = "Join lobby";
+  joinButton.id = lobby.lobbyId;
+
+  lobbyElement.appendChild(lobbyId);
+  lobbyElement.appendChild(joinButton);
+  document.body.appendChild(lobbyElement);
+
+  attachListener(lobbyElement);
+
+  return lobbyElement;
+}
+
+function attachListener(lobbyElement) {
+
+  const button = lobbyElement.querySelector("button");
+
+  button.addEventListener('click', () => {
+
+    window.location = `/${gameName}-lobby-${button.id}`;
+  });
+}
--- srv/app.js
+++ srv/app.js
@@ -13,32 +13,83 @@ app.use(Router.getPublicPath(''));
 
 app.get("/", Router.render("home", { games: Object.keys(GameRooms) }));
 
+const lobbies = {};
 
 app.get(/\/\w+-lobbies/, Router.render("lobbies", url => {
 
-  return { gameName: url.match(/(?<=\/)(\w*)(?=-lobbies)/)[0] };
-}));
+  const [gameName] = url.match(/(?<=\/)(\w*)(?=-lobbies)/);
 
-const lobbies = {};
+  return { gameName, lobbies: lobbies[gameName].lobbies };
+}));
 
 
 app.get(/\/\w+-lobby/, Router.render("lobby", url => {
 
-  return { gameName: url.match(/(?<=\/)(\w*)(?=-lobby)/)[0] };
+  const [gameName] = url.match(/(?<=\/)(\w*)(?=-lobby)/);
+  const [id] = url.match(/(?<=lobby\-)((\w|\-)*)/);
+
+  return { gameName, id};
 }));
 
+app.post(/\/create-lobby\/\w+/, (req, res) => {
 
-app.post("/lobbies", (req, res) => {
-  const lobbyId = uuid();
-  debug('lobby created', lobbyId);
-  lobbies[lobbyId] = {}
+  const [gameName] = req.url.match(/(?<=create-lobby\/)(\w*)/);
+
+  const lobbyId = lobbies[gameName].createLobby();
+
   res.send(JSON.stringify({ lobbyId }));
 });
+
+
+class GameLobbies {
+
+  constructor(gameName) {
+
+    debug(`Creating GameLobbies: ${gameName}`);
+    this.name = gameName;
+    this.lobbies = {};
+  }
+
+  createLobby() {
+
+    debug(`Creating lobby for ${this.name}`);
+
+    const lobbyId = uuid();
+
+    this.lobbies[lobbyId] = new GameLobby(this.name, lobbyId);
+
+    return lobbyId;
+  }
+
+  getLobby(lobbyId) {
+
+    return this.lobbies[lobbyId];
+  }
+
+  deleteLobby(lobbyId) {
+
+    delete this.lobbies[lobbyId];
+  }
+}
+
+class GameLobby {
+
+  open = true;
+  players = [];
+
+  constructor(gameName, lobbyId) {
+
+    this.name = gameName;
+    this.id = lobbyId;
+  }
+}
 
 
 for (const gameName in GameRooms) {
 
   const gameRouter = new GameRouter(gameName, GameRooms[gameName], io);
+
+  lobbies[gameName] = new GameLobbies(gameName);
 
   app.get(`/${gameName}`,
     gameRouter.createRoomIfNotExists.bind(gameRouter),
